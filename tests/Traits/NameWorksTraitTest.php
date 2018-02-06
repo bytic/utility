29bc5c03ef58afac0c0297bdaaf98f1186ff263b
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-02-06 12:37:52 +0200

    add tests

---
+++ tests/Traits/NameWorksTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Nip\Utility\Tests\Traits;
+
+use Nip\Utility\Traits\NameWorksTrait;
+
+/**
+ * Class NameWorksTraitTest
+ * @package Nip\Utility\Tests\Traits
+ */
+class NameWorksTraitTest extends \Nip\Utility\Tests\AbstractTest
+{
+    use NameWorksTrait;
+
+    public function testGetClassName()
+    {
+        self::assertSame('Nip\Utility\Tests\Traits\NameWorksTraitTest', $this->getClassName());
+
+        $name = 'Userrs';
+        $this->setClassName($name);
+        self::assertSame($name, $this->getClassName());
+    }
+
+    public function testIsNamespaced()
+    {
+        self::assertTrue($this->isNamespaced());
+    }
+
+    public function testGetNamespaceParentFolder()
+    {
+        self::assertSame('Traits', $this->getNamespaceParentFolder());
+        self::assertSame(
+            ['Nip', 'Utility', 'Tests', 'Traits', 'NameWorksTraitTest'],
+            $this->getClassNameParts()
+        );
+    }
+}
