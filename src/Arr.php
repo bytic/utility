7c418c316ab83b0e97d12938a1a1b19eeb00a62d
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-01-04 23:16:53 +0200

    add only to array

--- src/Arr.php
+++ src/Arr.php
@@ -81,6 +81,18 @@ class Arr
     }
 
     /**
+     * Get a subset of the items from the given array.
+     *
+     * @param array $array
+     * @param array|string $keys
+     * @return array
+     */
+    public static function only($array, $keys)
+    {
+        return array_intersect_key($array, array_flip((array)$keys));
+    }
+
+    /**
      * Pluck an array of values from an array.
      *
      * @param array $array
