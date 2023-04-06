1dd98c346579634f86c92c81bf936ab00e9c6a0e
Gabriel Solomon <hello@gabrielsolomon.ro>
2023-04-06 22:39:49 +0300

    fix: Str validate string

--- src/Str.php
+++ src/Str.php
@@ -367,6 +367,10 @@ class Str
      */
     public static function contains($haystack, $needles)
     {
+        $haystack = (string)$haystack;
+        if (empty($haystack)) {
+            return false;
+        }
         foreach ((array)$needles as $needle) {
             if ($needle != '' && mb_strpos($haystack, $needle) !== false) {
                 return true;
